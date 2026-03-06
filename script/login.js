2880b75b385b49a3559e141b660df1fbb80e04e7
md-morsalin10 <mdmorsalinofficial10@gmail.com>
2026-03-07 02:36:36 +0600

    login functionality completed and home page created

---
+++ script/login.js
@@ -0,0 +1,20 @@
+document.getElementById("login-btn").addEventListener("click", ()=>{
+    // console.log("clicked")
+    const userName = document.getElementById("user-name");
+    const name = userName.value;
+    console.log(name);
+    const userPassword = document.getElementById("password");
+    const pin = userPassword.value;
+    console.log(pin);
+
+    if(name == "admin" && pin == "admin123"){
+        alert("login successful")
+        window.location.assign("./home.html");
+    }
+    else{
+        alert("login Failed")
+        return;
+    }
+    
+    
+})
